cacd28fe219990ccd8429daf0ee15a58bf8a94ca
xymh <1135434503@qq.com>
2018-08-19 19:46:57 +0800

    开发公共工具类

--- day58-Mybatis-test04-cache/src/main/java/cn/itcast/domain/User.java
+++ day58-Mybatis-test04-cache/src/main/java/cn/itcast/domain/User.java
@@ -85,6 +85,8 @@ public class User implements Serializable {
 	public void setUserUsername(String userUsername) {
 		this.userUsername = userUsername;
 		System.out.println("张三用户管理模块");
+
+		System.out.println("李四我开发了一个工具类");
 	}
 
 }
